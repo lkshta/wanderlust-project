8fc664e32871aac150defa233064311a033576ad
Lakshita <2003lakshita@gmail.com>
2024-08-03 16:47:40 +0530

    Add Updated Files

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+
 const multer = require("multer");
 const { storage } = require("../cloudConfig.js");
 
